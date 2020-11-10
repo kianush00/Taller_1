2abfa26ac38ec9f434f59c719625a4b43f78c0b9
Kianush Atighi-Moghaddam <64434787+kianush00@users.noreply.github.com>
2020-11-10 09:21:54 -0300

    se agrega un método

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(definirNumeroAleatorioEntre(1,3));
-        System.out.println(definirNumeroAleatorioEntre(1,3));
-        System.out.println(definirNumeroAleatorioEntre(1,3));
-        System.out.println(definirNumeroAleatorioEntre(1,3));
+        
     }
 
     public static int definirNumeroAleatorioEntre(int min, int max){
-        return (int) (Math.random() * (max - min)) + min;
+        return (int) (Math.random() * ((max + 1) - min)) + min;
     }
 
     public static void imprimirCantidadProductos(){
